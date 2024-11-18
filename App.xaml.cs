31392cbc44b350b42bde22282880a393ac58b434
Eduardo Delben <eduardoodelben@gmail.com>
2024-11-17 22:42:36 -0300

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
